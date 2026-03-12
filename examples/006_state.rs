7def151d171674bfb98ad9866597896b528c88c8
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2026-03-12 14:12:18 -0300

    Remove top-level indentation in 006_state.rs

--- examples/006_state.rs
+++ examples/006_state.rs
@@ -8,30 +8,30 @@ use dyncord::commands::context::CommandContext;
 use dyncord::events::{EventContext, MessageCreate, On};
 use twilight_gateway::Intents;
 
-    #[tokio::main]
-    async fn main() {
-        let bot = Bot::new(CounterState::default())
-            .with_prefix("!")
-            .intents(Intents::GUILD_MESSAGES)
-            .intents(Intents::MESSAGE_CONTENT)
-            .command(Command::build("count", count_command))
-            .command(Command::build("reset", reset_command))
-            .on_event(On::message_create(on_message));
+#[tokio::main]
+async fn main() {
+    let bot = Bot::new(CounterState::default())
+        .with_prefix("!")
+        .intents(Intents::GUILD_MESSAGES)
+        .intents(Intents::MESSAGE_CONTENT)
+        .command(Command::build("count", count_command))
+        .command(Command::build("reset", reset_command))
+        .on_event(On::message_create(on_message));
 
-        bot.run(env::var("TOKEN").unwrap()).await;
-    }
+    bot.run(env::var("TOKEN").unwrap()).await;
+}
 
-    async fn on_message(ctx: EventContext<CounterState, MessageCreate>) {
-        ctx.state.counter.fetch_add(1, Ordering::SeqCst);
-    }
+async fn on_message(ctx: EventContext<CounterState, MessageCreate>) {
+    ctx.state.counter.fetch_add(1, Ordering::SeqCst);
+}
 
-    async fn count_command(ctx: CommandContext<CounterState>) {
-        let count = ctx.state.counter.load(Ordering::SeqCst);
+async fn count_command(ctx: CommandContext<CounterState>) {
+    let count = ctx.state.counter.load(Ordering::SeqCst);
 
-        ctx.reply(format!("Message count: {}", count))
-            .await
-            .unwrap();
-    }
+    ctx.reply(format!("Message count: {}", count))
+        .await
+        .unwrap();
+}
 
 async fn reset_command(ctx: CommandContext<CounterState>) {
     ctx.state.counter.store(0, Ordering::SeqCst);
